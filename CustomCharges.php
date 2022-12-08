f8778bd9502bd0bdabf4e9a39bd9bf6c4db4f5da
ihabz <ihab.zeedia@stanford.edu>
2022-12-08 14:56:23 -0800

    handle redcap instances without RIT dashboard.

--- CustomCharges.php
+++ CustomCharges.php
@@ -266,7 +266,7 @@ class CustomCharges extends \ExternalModules\AbstractExternalModule
     /**
      * @return ProjectPortal
      */
-    public function getR2p2DashboardObject(): ProjectPortal
+    public function getR2p2DashboardObject(): ProjectPortal|null
     {
         return $this->r2p2DashboardObject;
     }
